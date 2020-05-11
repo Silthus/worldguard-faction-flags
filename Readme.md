eadf5e39097f822e2815b8da3d3b1ad0ad21a711
Michael Reichenbach <michael@reichenbach.in>
2020-05-11 10:09:06 +0200

    feat(wg-flags): add factionc-claim flag
    * Added a custom WorldGuard flag `faction-claim` allow/deny
    * Setting the flag to deny will prevent claiming of chunks
    that intersect with the given WorldGuard region

--- Readme.md
+++ Readme.md
@@ -13,20 +13,37 @@
 This was requested by @jxyrod.
 
 **Short Description**
+
 I’m running a 1.7.10 Bungecord/Spigot server that is using massivecore factions v2.6.0 and worldguard v6.1 and I need a plugin that disables /f claiming and /f autoclaiming on worldguarded regions. Factions can claim up to the region but I don’t want them to be able to claim inside the region. Also if the worldguarded region doesn’t sit equally in a chunk I don't want the player to be able to /f claim and have the claim overlap into the worldguarded region.
 
 **Reference Plugins**
+
 I tried looking into my factions config but worldguard has not been integrated with this version of factions.
 
 **Detailed Feature Description**
-Describe each feature you want in detail, including commands, their description, permissions, and so on. Use this checklist to make sure you got everything:
-•	Short name of the feature: Disable faction claiming in region
-•	Description of the feature in the form of: When I /region flag [region] claiming deny then it should disable faction claiming in that region
-•	Needed permission for the feature.- Admin/OP just like regular worldguard
-•	Commands triggering the feature.- just a feature to disable claiming in regions for worldguard. It doesnt even need to have a command if you just have a plugin that disables claiming from all regions.
-•	Restrictions or other edge cases, e.g. should not be usable in creative mode. - None at the moment
-•	API integration with other plugins, e.g. - Massivecore Factions v2.6.0
 
+Describe each feature you want in detail, including commands, their description, permissions, and so on. Use this checklist to make sure you got everything:
+
+* Short name of the feature: Disable faction claiming in region
+* Description of the feature in the form of: When I /region flag [region] claiming deny then it should disable faction claiming in that region
+* Needed permission for the feature.- Admin/OP just like regular worldguard
+* Commands triggering the feature.- just a feature to disable claiming in regions for worldguard. It doesnt even need to have a command if you just have a plugin that disables claiming from all regions.
+* Restrictions or other edge cases, e.g. should not be usable in creative mode. - None at the moment
+* API integration with other plugins, e.g. - Massivecore Factions v2.6.0
+
+## Usage
+
+This plugin only adds a custom flag to the existing WorldGuard infrastructure. You can simply apply the new flag as you would any other WorldGuard flag. See the [WorldGuard documentation](https://worldguard.enginehub.org/en/latest/regions/flags/) for details.
+
+```shell script
+/region flag your-region faction-claim deny
+```
+
+Currently this plugin provides the following custom flags.
+
+| Flag | Type | Description |
+| ---- | ---- | ----------- |
+| `faction-claim` | `deny/allow` | Denys the claiming of faction chunks inside the given region if set to deny. *Defaults to: **allow*** |
 
 ## Supported Minecraft Versions
 
@@ -34,7 +51,7 @@ Describe each feature you want in detail, including commands, their description,
 | ------- | :-----: |
 | 1.7.10  |   ✔️    |
 
-## Project
+## Compiling the Project
 
 > **Note**: This project is compiled for a very old version of Minecraft.
 
@@ -42,6 +59,13 @@ You need to [download the Spigot build](https://cdn.getbukkit.org/spigot/spigot-
 
 ```shell script
 mvn install:install-file -Dfile=".\spigot-1.7.10-SNAPSHOT-b1657.jar" -DgroupId="org.spigot" -DartifactId="spigot-api" -Dversion="1.7.10-b1657" -Dpackaging=jar
+```
+
+You will also have to [download Factions](https://www.spigotmc.org/resources/factions.1900/) and install it into your local maven repository.
+
+```shell script
+mvn install:install-file -Dfile=".\Factions.jar" -DgroupId="com.massivecraft.factions" -DartifactId="Factions" -Dversion="2.14.0" -Dpackaging=jar
+mvn install:install-file -Dfile=".\MassiveCore.jar" -DgroupId="com.massivecraft" -DartifactId="core" -Dversion="2.14.0" -Dpackaging=jar
 ```
 
 Please read the [Contributing Guidelines](CONTRIBUTING.md) before submitting any pull requests or opening issues.
--- src/main/java/net/silthus/wgfactionflags/WorldGuardFactionFlagsPlugin.java
+++ src/main/java/net/silthus/wgfactionflags/WorldGuardFactionFlagsPlugin.java
@@ -1,30 +1,101 @@
 package net.silthus.wgfactionflags;
 
-import java.io.File;
+import com.massivecraft.factions.event.EventFactionsChunksChange;
+import com.sk89q.worldedit.BlockVector;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import lombok.Getter;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
+import java.io.File;
+
 public class WorldGuardFactionFlagsPlugin extends JavaPlugin implements Listener {
 
-  public WorldGuardFactionFlagsPlugin() {}
+    private static final String FACTION_CLAIM_FLAG_NAME = "faction-claim";
+    public static StateFlag FACTION_CLAIM_FLAG;
 
-  public WorldGuardFactionFlagsPlugin(
-      JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
-    super(loader, description, dataFolder, file);
-  }
+    @Getter
+    private WorldGuardPlugin worldGuard;
 
-  @Override
-  public void onEnable() {
+    public WorldGuardFactionFlagsPlugin() {
+    }
 
-    getServer().getPluginManager().registerEvents(this, this);
-  }
+    public WorldGuardFactionFlagsPlugin(
+            JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+    }
 
-  @EventHandler
-  public void onPlayerJoin(PlayerJoinEvent event) {
-    getLogger().info("Player joined.");
-  }
+    @Override
+    public void onLoad() {
+
+        super.onLoad();
+
+        this.worldGuard = getPlugin(WorldGuardPlugin.class);
+
+        FlagRegistry registry = worldGuard.getFlagRegistry();
+
+        try {
+            // create a flag with the name "my-custom-flag", defaulting to true
+            StateFlag flag = new StateFlag(FACTION_CLAIM_FLAG_NAME, true);
+            registry.register(flag);
+            FACTION_CLAIM_FLAG = flag; // only set our field if there was no error
+            getLogger().info("Sucessfully registered WorldGuard custom flag \"" + FACTION_CLAIM_FLAG_NAME + "\".");
+        } catch (FlagConflictException e) {
+            // some other plugin registered a flag by the same name already.
+            // you can use the existing flag, but this may cause conflicts - be sure to check type
+            Flag<?> existing = registry.get(FACTION_CLAIM_FLAG_NAME);
+            if (existing instanceof StateFlag) {
+                FACTION_CLAIM_FLAG = (StateFlag) existing;
+                getLogger().warning("Found existing WorldGuard flag with the name \"" + FACTION_CLAIM_FLAG_NAME + "\". Using this flag, but be aware that things may not work correctly.");
+            } else {
+                // types don't match - this is bad news! some other plugin conflicts with you
+                // hopefully this never actually happens
+                getLogger().severe("Failed to register custom flag \"" + FACTION_CLAIM_FLAG_NAME + "\"! A flag with the same name already exists.");
+                getLogger().severe("Disabling...");
+                setEnabled(false);
+            }
+        }
+    }
+
+
+    @Override
+    public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onFactionChunkClaim(EventFactionsChunksChange event) {
+
+        LocalPlayer player = getWorldGuard().wrapPlayer(event.getMPlayer().getPlayer());
+
+        event.getChunks().stream().map(ps -> ps.asBukkitChunk()).forEach(chunk -> {
+
+            RegionManager regionManager = getWorldGuard().getRegionManager(chunk.getWorld());
+
+            int bx = chunk.getX() << 4;
+            int bz = chunk.getZ() << 4;
+            BlockVector pt1 = new BlockVector(bx, 0, bz);
+            BlockVector pt2 = new BlockVector(bx + 15, 256, bz + 15);
+            ProtectedCuboidRegion region = new ProtectedCuboidRegion("ThisIsAnId", pt1, pt2);
+            ApplicableRegionSet regions = regionManager.getApplicableRegions(region);
+
+            if (!regions.testState(player, FACTION_CLAIM_FLAG)) {
+                event.getMPlayer().message(ChatColor.DARK_RED + "You cannot claim chunks inside this WorldGuard region.");
+                event.setCancelled(true);
+            }
+        });
+    }
 }
